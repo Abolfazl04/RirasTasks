1e79f39329129b75d9b8fc7b4278e33a29fea1b0
Abolfazl04 <a.moradzadehfard@gmail.com>
2025-02-24 11:39:02 +0330

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Threading.Tasks;
+using static System.Environment;
+using RirasTasks;
+//Creating an enum for categories as asked 
+public enum Categories
+{
+    Category1,
+    Category2,
+    Category3
+}
+
+//Creating a class for our Products so we can save and access them easily 
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public Categories Category { get; set; }
+    public decimal Price { get; set; }
+}
+
+public class Program1
+{
+    public static void Main()
+    {
+        //The given piece of code that creates a list of "Product" called "products"
+        List<Product> products = new List<Product>
+                {
+                    new Product { Id = 1, Name = "Product A", Category = Categories.Category1, Price = 100 },
+                    new Product { Id = 2, Name = "Product B", Category = Categories.Category1, Price = 150 },
+                    new Product { Id = 3, Name = "Product C", Category = Categories.Category2, Price = 120 },
+                    new Product { Id = 4, Name = "Product D", Category = Categories.Category3, Price = 200 },
+                    new Product { Id = 5, Name = "Product E", Category = Categories.Category1, Price = 80 }
+                };
+
+        //I used this space to call my functions that are in "Tasks.cs" and output the results
+        Console.WriteLine(NewLine + "Task one : ");
+        Tasks.T1(products);
+
+        Console.WriteLine("-----------------" + NewLine + "Task Two : ");
+        Tasks.T2(products);
+
+        Console.WriteLine("-----------------" + NewLine + "Task Three : ");
+        Tasks.T3(products);
+
+        Console.WriteLine("-----------------" + NewLine + "Task Four : ");
+        Tasks.T4(products);
+
+        Console.WriteLine("-----------------" + NewLine + "Task Five : ");
+        Tasks.T5(products);
+
+    }
+
+
+
+
+
+}
---
+++ Tasks.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Environment;
+
+namespace RirasTasks
+{
+    internal class Tasks
+    {
+        //This file is used to calculate and output the tasks results 
+        
+        
+        public static void T1(List<Product> products)
+        {
+            //This line searches through the list and finds every Product that is in Category1 
+            //then saves them inside "Results" 
+            var Results = products.Where(c => c.Category == Categories.Category1);    
+            
+            //Outputting the results 
+            foreach (var i in Results)
+            {
+                Console.WriteLine(i.Name);
+            }
+        }
+
+        public static void T2(List<Product> products)
+        {
+            //Using .Max can find the highest value in a list so we can find the highest price 
+            decimal MostExpensive =(products.Max(x => x.Price));
+            //Outputting the founded Product
+            Console.WriteLine("The most expensive product is : " + MostExpensive);
+        }
+
+        public static void T3(List<Product> products)
+        {   
+            //using .Sum we can find this Answer 
+            double sum = Convert.ToDouble(products.Sum(x => x.Price));
+            //Outputting the total sum of prices 
+            Console.WriteLine("The total sum is : " + sum);
+        }
+        
+        public static void T4(List<Product> products)
+        {
+            //I sorted the products by categories using .OrderBy 
+            //It will not be necessary if used with the next lines but if we are just going to sort 
+            //and not display them we have to use it 
+            products = products.OrderBy(x => x.Category).ToList();
+            
+            //The first loop is to find every type of categories that we have 
+            foreach (Categories category in Enum.GetValues(typeof(Categories))) 
+            {
+                Console.WriteLine(category + " :");
+                //Inside the second loop every products category is being compared to the
+                //category in first loop, if matched it will be outputted 
+                foreach (var product in products)
+                    if (product.Category == category)
+                        Console.WriteLine(product.Name);
+            }
+        }
+        public static void T5(List<Product> products)
+        {
+            //Using .Average we can easily find the average of the prices
+            double avg = Convert.ToDouble(products.Average(x => x.Price));
+            Console.WriteLine("Average Price is : " + avg);
+        }
+    }
+}
